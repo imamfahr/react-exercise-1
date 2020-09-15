b2cd99e26edeb60a032957351b370f96e4ae7c9f
imamfahr <imamfahr@gmail.com>
2020-09-15 14:20:12 +0700

    adding style

--- my-app/src/Component/Product.js
+++ my-app/src/Component/Product.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './ListProduct.css'
 
 const Food = (props) => {
   return (
-    <div>
+    <div className="list-food-cotainer">
       <h3>{props.name}</h3>
       <h3>{props.price}</h3>
     </div>
